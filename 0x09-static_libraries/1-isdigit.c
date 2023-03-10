441c301e85873e1cfdbeca18d9a8292ae6617946
Youmnaelghanam <youmnaelghanam226@gmail.com>
2023-03-10 11:12:49 -0800

    update

--- 0x09-static_libraries/1-isdigit.c
+++ 0x09-static_libraries/1-isdigit.c
@@ -1,9 +1,19 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
+
+/**
+* _isdigit - checks for a digit from 0 to 9
+* @c: character to check
+* Return: 1 if a digit and 0 otherwise
+*/
+
 int _isdigit(int c)
 {
-return isdigit(c);
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
