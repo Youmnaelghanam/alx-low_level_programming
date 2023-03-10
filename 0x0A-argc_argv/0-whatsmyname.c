e55538f81138cfb7dd36404426b165efa2531d49
Youmnaelghanam <youmnaelghanam226@gmail.com>
2023-03-10 04:47:36 -0800

    update

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 
 /**
@@ -14,20 +13,4 @@ int main(int argc, char **argv)
 
 	printf("%s\n", argv[0]);
 	return (0);
-=======
-#include <stdio.h>                                                                                                                   
-#include "main.h"                                                                                                                                                                                                                                                         
-/**                                                                                                                                  
- * main - Print the name of the program                                                                                              
- * @argc: Count arguments                                                                                                            
- * @argv: Arguments                                                                                                                  
- *                                                                                                                                   
- * Return: Always 0 (Success)                                                                                                        
- */                                                                                                                                                                                                                                                                      
-int main(int argc, char *argv[])                                                                                                     
-{                                                                                                                                    
-(void) argc;                                                                                                                         
-printf("%s\n", argv[0]);                                                                                                                                                                                                                                               
-return (0);                                                                                                                          
->>>>>>> 556c136cc96f9521f7d6589a60af979fcaea570b
 }
